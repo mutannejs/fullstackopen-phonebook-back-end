63322fef2845da72c8a95d00947d7e4d9c7f1df7
mutannejs <murillojustinoklein@gmail.com>
2025-08-13 06:35:21 -0300

    exercício 3.11

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ morgan.token('body', function getBody (req, res) {
 const app = express();
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
