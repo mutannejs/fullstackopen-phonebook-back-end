f276d5685037f95115d54770119c7a488b604a5f
mutannejs <murillojustinoklein@gmail.com>
2025-08-12 21:34:49 -0300

    exercício 3.9

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -32,6 +33,8 @@ morgan.token('body', function getBody (req, res) {
 });
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
