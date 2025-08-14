012ebf19bbcaa2b80e87dd0c74e8134aaca5d04c
mutannejs <murillojustinoklein@gmail.com>
2025-08-14 19:59:45 -0300

    3.13

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
+import { Person } from './models/person.js';
+
 const PORT = process.env.PORT || 3001;
 const MAX_RANDOM_NUMBER = 999999;
 
-const persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// const persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ]
 
 morgan.token('body', function getBody (req, res) {
   return JSON.stringify(req.body)
@@ -40,7 +43,9 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (req, resp) => {
-  return resp.json(persons);
+  Person.find({}).then(
+    response => resp.json(response)
+  );
 });
 
 app.get('/api/persons/:id', (req, resp) => {
